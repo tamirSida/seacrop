7efc3dc5070508d4c8f1353494ce6df6ddb2e4fb
Tamir Sida <tamir.sida@post.runi.ac.il>
2025-08-29 19:11:04 +0300

    seo

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  trailingSlash: false,
+  skipTrailingSlashRedirect: true,
+  async redirects() {
+    return [
+      {
+        source: '/index.html',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'index, follow',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
